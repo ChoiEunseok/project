aa36a9c8ee6aac1a9e4c572b5397525a4dc761dd
eunseok <dmstjr0216@naver.com>
2024-02-21 00:31:20 +0900

    'first'

---
+++ src/main/java/com/kh/projects/web/PubdataController.java
@@ -0,0 +1,36 @@
+package com.kh.projects.web;
+
+
+import com.kh.projects.domain.pubdata.NaverNews;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Slf4j
+@Controller
+@RequestMapping("/pubdata")
+@RequiredArgsConstructor // final필드를 매개변수로하는 생성자를 자동 만들어줌
+public class PubdataController {
+
+  private final NaverNews naverNews;
+
+  @GetMapping("/news")      // get http://localhost:9080/pubdata/news
+  public String news(){
+    return "pubdata/news";
+  }
+
+  @ResponseBody
+  @GetMapping("/news/search")
+  public String search(
+          @RequestParam("keyword") String keyword
+  ){
+    log.info("keyword={}",keyword);
+    String data = naverNews.reqNews(keyword);
+    return data;
+  }
+
+}
